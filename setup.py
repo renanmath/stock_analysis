241c2845fb269476cb8ff0234243abb4d69b81ca
renanmath <renanmath13@gmail.com>
2023-01-17 23:21:24 -0300

    add requirements and setup

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='brfundamentus',
+    version='0.0.1',
+    packages=find_packages(),
+    url='https://github.com/renanmath/stock_analysis',
+    license='MIT',
+    author='renan',
+    author_email='renanmath13@gmail.com',
+    description='A package to handle fundamental analysis of Brazilian stocks'
+)
+
