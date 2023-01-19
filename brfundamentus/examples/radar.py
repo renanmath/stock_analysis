3a9bbb57c967c2dfa50aa9dd8bbea84640c9b6aa
renanmath <renanmath13@gmail.com>
2023-01-18 21:50:56 -0300

    create radar function

--- brfundamentus/examples/radar.py
+++ brfundamentus/examples/radar.py
@@ -21,71 +21,74 @@ def union_lists(list1: list, list2: list) -> list:
 
     return union
 
+def radar(path_to_csv: str, path_to_parameters:str):
+    # First, instantiate a StockMarket object
+    # This class is responsible to perform the valuations
+    # and has some useful filter methods
+    market = StockMarket.read_from_csv(path_to_csv)
 
-# First, instantiate a StockMarket object
-# This class is responsible to perform the valuations
-# and has some useful filter methods
-market = StockMarket.read_from_csv('statusinvest-busca-avancada.csv')
-print(len(market.stocks))
-
-# Then, define your own filters
-# They have to be dictionaries of three keys:
-# parameter, cut_criterion and reverse_cut.
-# See the docstring of method get_top_stocks_by_conditions
-# In this example, we will pull the parameters from a json file
-# named radar_parameters.json
-# In the repository we include a radar_parameters_example.json
-# Copy this file and change its name to radar_parameters.json
-# then set your own personal parameters
+    # Then, define your own filters
+    # They have to be dictionaries of three keys:
+    # parameter, cut_criterion and reverse_cut.
+    # See the docstring of method get_top_stocks_by_conditions
+    # In this example, we will pull the parameters from a json file
+    # named radar_parameters.json
+    # In the repository we include a radar_parameters_example.json
+    # Copy this file and change its name to radar_parameters.json
+    # then set your own personal parameters
 
 
-with open('brfundamentus/examples/radar_parameters.json') as file:
-    parameters = json.load(file)
+    with open(path_to_parameters) as file:
+        parameters = json.load(file)
 
-filtros_comuns = parameters['common']
+    filtros_comuns = parameters['common']
 
-filtros = parameters['first']
+    filtros = parameters['first']
 
-filtros += filtros_comuns
+    filtros += filtros_comuns
 
-rank = {'parameter': 'graham_valuation', 'ascending': False}
+    rank = {'parameter': 'graham_valuation', 'ascending': False}
 
-initial_list = market.get_top_stocks_by_list_of_conditions(
-    conditions=filtros, sort_by=rank, num_stocks=600
-)
-
-print('\nPrimeira lista')
-print_list(initial_list)
-print(len(initial_list))
-
-# second list
-
-filtros = parameters['second']
-
-filtros += filtros_comuns
-
-second_list = union_lists(
-    initial_list,
-    market.get_top_stocks_by_list_of_conditions(
+    initial_list = market.get_top_stocks_by_list_of_conditions(
         conditions=filtros, sort_by=rank, num_stocks=600
-    ),
-)
+    )
 
-print('\nSegunda lista')
-print_list(second_list)
-print(len(second_list))
+    print('\nPrimeira lista')
+    print_list(initial_list)
+    print(len(initial_list))
 
-filtros = parameters['third']
+    # second list
 
-filtros += filtros_comuns
+    filtros = parameters['second']
 
-third_list = union_lists(
-    second_list,
-    market.get_top_stocks_by_list_of_conditions(
-        conditions=filtros, sort_by=rank, num_stocks=600
-    ),
-)
+    filtros += filtros_comuns
 
-print('\nTerceira lista')
-print_list(third_list)
-print(len(third_list))
+    second_list = union_lists(
+        initial_list,
+        market.get_top_stocks_by_list_of_conditions(
+            conditions=filtros, sort_by=rank, num_stocks=600
+        ),
+    )
+
+    print('\nSegunda lista')
+    print_list(second_list)
+    print(len(second_list))
+
+    filtros = parameters['third']
+
+    filtros += filtros_comuns
+
+    third_list = union_lists(
+        second_list,
+        market.get_top_stocks_by_list_of_conditions(
+            conditions=filtros, sort_by=rank, num_stocks=600
+        ),
+    )
+
+    print('\nTerceira lista')
+    print_list(third_list)
+    print(len(third_list))
+
+
+if __name__ == '__main__':
+    radar('statusinvest-busca-avancada.csv', 'brfundamentus/examples/radar_parameters.json')
